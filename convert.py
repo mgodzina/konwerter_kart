95ff7c3a9150150a47a67bd5e4036a1d52bf369d
Marcin Godzina <61230434+mgodzina@users.noreply.github.com>
2023-05-29 11:08:21 +0200

    Update convert.py

--- convert.py
+++ convert.py
@@ -1,37 +1,51 @@
 import csv
 import operator
+from datetime import date
+import glob
+import os
 
-# numery grup dla kontroli dostępu
+
 GRUPA_PELNY = 1
 GRUPA_PROWADZACY = 2
 GRUPA_KLUBOWICZ = 3
 GRUPA_BEZDOSTEPU = 250
+dostep_pelny = 0
+dostep_prowadzacy = 0
+dostep_klubowicz = 0
 
 
-# konwersja numeru karty na format hex oraz dodanie nagłówka zależnego od grupy kart
+
 def karta(numer):
-    front = "0000"
+    front= ""
+    if int(numer) > 11000000 and int(numer) < 12000000:
+        front = "1D00"
     if int(numer) > 2440000 and int(numer) < 4600000:
         front = "5500"
+    if int(numer) > 3000000 and int(numer) < 3170000:
+        front = "1C00"
     if int(numer) > 5860000 and int(numer) < 6000000:
         front = "8800"
+    if int(numer) > 7400000 and int(numer) < 7600000:
+        front = "3100"
     if int(numer) > 8049000 and int(numer) < 8280000:
         front = "1700"
-    return front + hex(int(numer))[2:].upper()
+    return front+hex(int(numer))[2:].upper()
 
-
-# przypisanie do grupy na podstawie uprawnień
 def grupa(row):
-    if row[1] == 't' and row[2] == 't' and row[3] == 't' and row[4] == 't':
+    global dostep_pelny
+    global dostep_prowadzacy
+    global dostep_klubowicz
+    if row[1] == 't' and row[2] == 't' and row[3] == 't' and row[4] == 't' or row[5] == 't':
+        dostep_pelny += 1
         return GRUPA_PELNY
     if row[1] == 't' and row[2] == 't' and row[3] == 't' and row[4] == 'f':
+        dostep_prowadzacy += 1
         return GRUPA_PROWADZACY
     if row[1] == 't' and row[2] == 't' and row[3] == 'f' and row[4] == 'f':
+        dostep_klubowicz += 1
         return GRUPA_KLUBOWICZ
-    return GRUPA_BEZDOSTEPU
+    return 0;
 
-
-# przypisanie numeru id użytkownika i zwiększenie licznika
 def numer(row):
     global prowadzacy
     global pracownicy
@@ -44,50 +58,71 @@ def numer(row):
         prowadzacy += 1
         return prowadzacy - 1
     czlonek += 1
-    return czlonek - 1
+    return  czlonek - 1
 
-
-# deklaracja zmiennych
 temp = []
 dane_input = []
 dane_output = []
-# startowe numery id dla użytkowników oraz licznik
 pracownicy = 10
 prowadzacy = 100
 czlonek = 1000
+user = 1
 
-# otwarcie pliku csv i dodanie do listy
-with open('kontrola_dostepu.csv') as csv_file:
+list_of_files = glob.glob('C:\\Users\\KSI\\Downloads\\kontrola_dostepu*.csv') # * means all if need specific format then *.csv
+latest_file = max(list_of_files, key=os.path.getctime)
+print(latest_file)
+
+
+with open(latest_file) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
     for row in csv_reader:
-        # zignoruj linie nagłówka
         if line_count == 0:
             line_count += 1
         else:
             dane_input.append(row)
             line_count += 1
+print(f'Processed {line_count} lines from data input')
+print(f'No. of corrent entries {len(dane_input)} lines.')
 
-# sprawdzenie czy nr karty jest liczbą
+with open('stale.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            line_count += 1
+        else:
+            dane_input.append(row)
+            line_count += 1
+print(f'Processed {line_count} lines from constant data input')
+print(f'No. of corrent entries {len(dane_input)} lines.')
+
+
 for row in dane_input:
     if row[0].isdigit() == False:
         temp.append(row)
 
-# usunięcie nieprawidłowych wpisów z listy
 for row in temp:
     dane_input.remove(row)
 
-# utworzenie listy w wyjściowym formacie
-user = 1
 for row in dane_input:
-    dane_output.append(
-        [numer(row), user, user, '', karta(row[0]), '', '48', grupa(row), 'False', 'True', '', '', '', '', '', ''])
+    dane_output.append([numer(row), user, user,'', karta(row[0]),'','48', grupa(row),'False','True','','','','','',''])
     user += 1
 
-# sortowanie listy
-export_list = sorted(dane_output, key=operator.itemgetter(0))
+print(f'No. of pracownicy = {pracownicy - 10}')
+print(f'No. of full access  = {dostep_pelny}')
+print(f'No. of 15m access = {dostep_prowadzacy}')
+print(f'No. of normal access = {dostep_klubowicz}')
 
-# eksport do pliku csv
-with open('export.csv', mode='w', newline="") as export_file:
-    export_writer = csv.writer(export_file, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    export_writer.writerows(export_list)
+export=sorted(dane_output, key=operator.itemgetter(0))
+
+
+today = date.today()
+exportfilename = today.strftime("export%Y%m%d.csv")
+
+
+with open(exportfilename, mode='w', newline="") as employee_file:
+    employee_writer = csv.writer(employee_file, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    employee_writer.writerows(export)
+
+input()
